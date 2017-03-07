4ac4e4821358a9a21871ff401df6455a08973e71
R. Mark Sharp <msharp@TxBiomed.org>
2017-03-07 10:18:49 -0600

    Added roxygen2 documentation

--- R/PedigreeSampling.R
+++ R/PedigreeSampling.R
@@ -3,75 +3,101 @@
 #
 # Contains functions to build pedigrees from sub-samples
 # of genotyped individuals.
-# 
+#
 # The goal of sampling is to reduce the number of inbreeding
 # loops in the resulting pedigree, and thus, reduce the
 # amount of time required to perform calculations with
 # SIMWALK2 or similar programs.
-# 
-###############################################################################
-# Data Definition:
-# 
-# PedTree
-# A list containing sire and dam information for an individual.
-# 
-###############################################################################
+#
+#' Create a pedigree tree (PedTree).
+#'
+#' The PedTree is a list containing sire and dam information for an individual.
+#'
+#' @param ped dataframe of pedigree and demographic information potentially
+#' containing columns indicating the birth and death dates of an individual.
+#' The table may also contain dates of sale (departure). Optional columns
+#' are \code{birth}, \code{death}, \code{departure}.
+#'
+#' @return A list of named lists forming a pedigree tree (PedTree or ptree).
+#' Each sublist represents an ID in the pedigree and contains the sire ID and
+#' the dam ID as named elements.
+#' @export
+createPedTree <- function(ped) {
 
-createPedTree <- function(ped){
-  
-  p <- rep(list(list(sire=NA, dam=NA)), nrow(ped))
+  p <- rep(list(list(sire = NA, dam = NA)), nrow(ped))
   names(p) <- ped$id
-  
-  for(i in 1:nrow(ped)){
+
+  for (i in 1:nrow(ped)) {
     p[[ped$id[i]]]$sire <- ped$sire[i]
     p[[ped$id[i]]]$dam <- ped$dam[i]
   }
   return(p)
 }
+#' Recursively create a character vector of ancestors for an individual ID.
+#'
+#' @param id character vector of length 1 having the ID of interest
+#' @param ptree a list of lists forming a pedigree tree as constructed by
+#' \code{createPedTree(ped)} where \code{ped} is a standard pedigree dataframe.
+#' @export
+getAncestors <- function(id, ptree) {
 
-getAncestors <- function(id, ptree){
-  
-  if(is.na(id)){
+  if (is.na(id)) {
     return(c())
   }
-  
+
   sire <- ptree[[id]]$sire
   dam <- ptree[[id]]$dam
-  
-  if(!is.na(sire)){
+
+  if (!is.na(sire)) {
     s_anc <- getAncestors(sire, ptree)
     sire_lineage <- c(sire, s_anc)
   } else{
     sire_lineage <- c()
   }
-  
-  if(!is.na(dam)){
+
+  if (!is.na(dam)) {
     d_anc <- getAncestors(dam, ptree)
     dam_lineage <- c(dam, d_anc)
   } else{
     dam_lineage <- c()
   }
-  
+
   return(c(sire_lineage, dam_lineage))
 }
-#####
-makesLoop <- function(id, ptree){
-  
+#' Tests to see if sires and dams for an individual in a ptree have a common
+#' ancester.
+#'
+#' @return TRUE if one or more common ancestor exists
+#' @param id character vector of length 1 having the ID of interest
+#' @param ptree a list of lists forming a pedigree tree as constructed by
+#' \code{createPedTree(ped)} where \code{ped} is a standard pedigree dataframe.
+#' @export
+makesLoop <- function(id, ptree) {
+
   s_anc <- getAncestors(ptree[[id]]$sire, ptree)
   d_anc <- getAncestors(ptree[[id]]$dam, ptree)
   overlap <- intersect(s_anc, d_anc)
-  
+
   return(length(overlap) > 0)
 }
+#' Find loops in a pedigree tree
+#'
+#' @return a named list of logical values where each named element is
+#' named with an \code{id} from \code{ptree}. The value of the list element
+#' is set to \code{TRUE} if the \code{id} has a loop in the pedigree.
+#' Loops occur when an animal's sire and dam have a common ancestor.
+#'
+#' @param ptree a list of lists forming a pedigree tree as constructed by
+#' \code{createPedTree(ped)} where \code{ped} is a standard pedigree dataframe.
+#' @export
+findLoops <- function(ptree) {
 
-findLoops <- function(ptree){
-  
   ids <- names(ptree)
   loops <- vector("list", length(ids))
   names(loops) <- ids
-  
-  for(id in ids){
-    if(makesLoop(id, ptree)){
+
+  for (id in ids) {
+    if (makesLoop(id, ptree)) {
       loops[[id]] <- TRUE
     } else{
       loops[[id]] <- FALSE
@@ -79,13 +105,27 @@ findLoops <- function(ptree){
   }
   return(loops)
 }
+#' Count the number of loops in a pedigree tree.
+#'
+#' Uses the \code{loops} data structure and the list of all ancestors for
+#' each individual to calculate the number of loops for each individual.
+#'
+#' @return A list indexed with each ID in the pedigree tree (\code{ptree})
+#' containing the number of loops for each individual.
+#'
+#' @param loops a named list of logical values where each named element is
+#' named with an \code{id} from \code{ptree}. The value of the list element
+#' is set to \code{TRUE} if the \code{id} has a loop in the pedigree.
+#' Loops occur when an animal's sire and dam have a common ancestor.
+#' @param ptree a list of lists forming a pedigree tree as constructed by
+#' \code{createPedTree(ped)} where \code{ped} is a standard pedigree dataframe.
+#' @export
+countLoops <- function(loops, ptree) {
 
-countLoops <- function(loops, ptree){
-  
   ids <- names(ptree)
   counts <- list()
-  
-  for(id in ids){
+
+  for (id in ids) {
     anc <- getAncestors(id, ptree)
     l <- loops[c(id, anc)]
     counts[id] <- sum(unlist(l))
--- R/Relations.R
+++ R/Relations.R
@@ -28,60 +28,59 @@
 #     Designate the relationships as 'no relation'
 
 ###############################################################################
+#' Make a CEPH-style pedigree for each id
+#'
+#' Creates a CEPH-style pedigree for each id, consisting of three generations:
+#' the id, the parents, and the grandparents. Inserts NA for unknown pedigree
+#' members.
+#'
+#' @param id character vector of IDs for a set of animals.
+#' @param id character vector with unique identifier for an individual
+#' @param sire character vector with unique identifier for an
+#' individual's father (\code{NA} if unknown).
+#' @param dam character vector with unique identifier for an
+#' individual's mother (\code{NA} if unknown).
+#'
+#' @return list of lists {fields: id, {subfields: parents, pgp, mgp}}
+#' Pedigree information converted into a CEPH-style list. The top level
+#' list elements are the IDs from id. Below each ID is a list of three
+#' elements: parents (sire, dam), paternal grandparents (pgp: sire, dam),
+#' and maternal grandparents (mgp: sire, dam).
 
-makeCEPH <- function(id, sire, dam){
-  # Creates a CEPH-style pedigree for each id, consisting of three generations:
-  # the id, the parents, and the grandparents. Inserts NA for unknown pedigree
-  # members.
-  # 
-  # Parameters
-  # ----------
-  # id : vector <char>
-  #   IDs for a set of animals.
-  # sire : vector <char or NA>
-  #   IDs of the sires for the set of animals.
-  # dam : vector <char or NA>
-  #   IDs of the dams for the set of animals.
-  # 
-  # Return
-  # ------
-  # list of lists {fields: id, {subfields: parents, pgp, mgp}}
-  #   Pedigree information converted into a CEPH-style list. The top level
-  #   list elements are the IDs from id. Below each ID is a list of three
-  #   elements: parents (sire, dam), paternal grandparents (pgp: sire, dam),
-  #   and maternal grandparents (mgp: sire, dam).
-  
-  ped <- data.frame(sire=sire, dam=dam, row.names=id, stringsAsFactors=FALSE)
-  
+
+makeCEPH <- function(id, sire, dam) {
+  ped <- data.frame(sire = sire, dam = dam, row.names = id,
+                    stringsAsFactors = FALSE)
+
   ceph <- list()
-  for(i in id){
+  for (i in id) {
     sire <- ped[i, "sire"]
     dam <- ped[i, "dam"]
     parents <- c(sire, dam)
-    
-    if(is.na(sire)){
+
+    if (is.na(sire)) {
       pgp <- c(NA, NA)
     } else{
       pgp <- c(ped[sire, "sire"], ped[sire, "dam"])
     }
-    
-    if(is.na(dam)){
+
+    if (is.na(dam)) {
       mgp <- c(NA, NA)
     } else{
       mgp <- c(ped[dam, "sire"], ped[dam, "dam"])
     }
-    
-    ceph[[i]] <- list(parents=parents, pgp=pgp, mgp=mgp)
+
+    ceph[[i]] <- list(parents = parents, pgp = pgp, mgp = mgp)
   }
-  
+
   return(ceph)
 }
 
-countFirstOrder <- function(ped, ids=NULL){
+countFirstOrder <- function(ped, ids = NULL) {
   # Tallies the number of first-order relatives for each member of the provided
   # pedigree. If 'ids' is provided, the analysis is restricted to only the
   # specified subset.
-  # 
+  #
   # Parameters
   # ----------
   # ped : `Pedigree`
@@ -91,35 +90,35 @@ countFirstOrder <- function(ped, ids=NULL){
   #   relationships will only be tallied for the listed IDs and will only
   #   consider relationships within the subset. If NULL, the analysis will
   #   include all IDs in the pedigree.
-  # 
+  #
   # Return
   # ------
   # data.frame {fields: id, parents, offspring, siblings, total}
   #   A table of first-order relationship counts, broken down to indicate
   #   the number of parents, offspring, and siblings that are part of the
   #   subset under consideration.
-  
-  if(!is.null(ids)){
+
+  if (!is.null(ids)) {
     ped <- ped[ped$id %in% ids, ]
   }
   rownames(ped) <- 1:nrow(ped)
   parents <- c()
   offspring <- c()
   siblings <- c()
-  
-  for(i in 1:nrow(ped)){
+
+  for (i in 1:nrow(ped)) {
     id <- ped[i, "id"]
     sire <- ped[i, "sire"]
     dam <- ped[i, "dam"]
-    
+
     p <- sum(c((sire %in% ped$id), (dam %in% ped$id)))
     o <- sum((ped$sire %in% id) | (ped$dam %in% id))
-    if(is.na(sire) | is.na(dam)){
+    if (is.na(sire) | is.na(dam)) {
       s <- 0
     } else{
       s <- sum((ped$sire %in% sire) & (ped$dam %in% dam)) - 1
     }
-    
+
     parents <- c(parents, p)
     offspring <- c(offspring, o)
     siblings <- c(siblings, s)
@@ -129,118 +128,123 @@ countFirstOrder <- function(ped, ids=NULL){
   ped["offspring"] <- offspring
   ped["siblings"] <- siblings
   ped["total"] <- total
-  
+
   return(ped[, c("id", "parents", "offspring", "siblings", "total")])
 }
 
-convertRelationships <- function(kmat, ped, ids=NULL, updateProgress=NULL){
-  # Converts pairwise kinship values to a relationship category descriptor.
-  # 
-  # Parameters
-  # ----------
-  # kmat : matrix
-  #   Matrix of pairwise kinship coefficients. Rows and columns
-  #   should be named with IDs.
-  # ped : `Pedigree`
-  #   Standardized pedigree information in a table.
-  # ids : vector <char> or NULL
-  #   List of IDs to which the analysis should be restricted. If provided,
-  #   only relationships between these IDs will be converted to categories.
-  # updateProgress : function or NULL
-  #   Function that can be called during each iteration to update a
-  #   shiny::Progress object.
-  # 
-  # Return
-  # ------
-  # data.frame {fields: id1, id2, kinship, relation}
-  #   Long-form table of pairwise kinships, with relationship categories
-  #   included for each pair.  
-  
-  if(!is.null(ids)){
+# Converts pairwise kinship values to a relationship category descriptor.
+#
+# Parameters
+# ----------
+# kmat : matrix
+#   Matrix of pairwise kinship coefficients. Rows and columns
+#   should be named with IDs.
+# ped : `Pedigree`
+#   Standardized pedigree information in a table.
+# ids : vector <char> or NULL
+#   List of IDs to which the analysis should be restricted. If provided,
+#   only relationships between these IDs will be converted to categories.
+# updateProgress : function or NULL
+#   Function that can be called during each iteration to update a
+#   shiny::Progress object.
+#
+# Return
+# ------
+# data.frame {fields: id1, id2, kinship, relation}
+#   Long-form table of pairwise kinships, with relationship categories
+#   included for each pair.
+
+convertRelationships <- function(kmat, ped, ids = NULL, updateProgress = NULL) {
+  if (!is.null(ids)) {
     kmat <- filterKinMatrix(ids, kmat)
   }
-  kin <- reformatKinship(kmat, rm.dups=TRUE)
+  kin <- reformatKinship(kmat, rm.dups = TRUE)
   ped <- makeCEPH(ped$id, ped$sire, ped$dam)
   r <- c()
-  
-  for(i in 1:nrow(kin)){
+
+  for (i in 1:nrow(kin)) {
     id1 <- kin$id1[i]
     id2 <- kin$id2[i]
-    
+
     ceph1 <- ped[[id1]]
     ceph2 <- ped[[id2]]
-    
-    if(id1 == id2){
+
+    if (id1 == id2) {
       relation <- "Self"
-    } else if(entire(ceph1$parents == ceph2$parents)){
+    } else if (entire(ceph1$parents == ceph2$parents)) {
       relation <- "Full-Siblings"
-    } else if(id1 %in% ceph2$parents | id2 %in% ceph1$parents){
+    } else if (id1 %in% ceph2$parents | id2 %in% ceph1$parents) {
       # one animal is the parent of the other
       relation <- "Parent-Offspring"
-    } else if(!isEmpty(intersect(ceph1$parents, ceph2$parents))){
+    } else if (!isEmpty(intersect(ceph1$parents, ceph2$parents))) {
       # at least 1 parent is shared
       relation <- "Half-Siblings"
-    } else if(id1 %in% c(ceph2$pgp, ceph2$mgp) | id2 %in% c(ceph1$pgp, ceph1$mgp)){
+    } else if (id1 %in% c(ceph2$pgp, ceph2$mgp) |
+               id2 %in% c(ceph1$pgp, ceph1$mgp)) {
       # one animals is the grandparent of the other
       relation <- "Grandparent-Grandchild"
-    } else if(entire(ceph1$pgp == ceph2$pgp) | entire(ceph1$pgp == ceph2$mgp) |
-              entire(ceph1$mgp == ceph2$pgp) | entire(ceph1$mgp == ceph2$mgp)){
+    } else if (entire(ceph1$pgp == ceph2$pgp) |
+               entire(ceph1$pgp == ceph2$mgp) |
+               entire(ceph1$mgp == ceph2$pgp) |
+               entire(ceph1$mgp == ceph2$mgp)) {
       # When a full set of grandparents are shared
       relation <- "Full-Cousins"
-    } else if(!isEmpty(intersect(c(ceph1$pgp, ceph1$mgp),
-                                 c(ceph2$pgp, ceph2$mgp)))){
+    } else if (!isEmpty(intersect(c(ceph1$pgp, ceph1$mgp),
+                                 c(ceph2$pgp, ceph2$mgp)))) {
       # When at least one grandparent is in common
       relation <- "Cousin - Other"
-    } else if(entire(ceph1$parents == ceph2$pgp) |
+    } else if (entire(ceph1$parents == ceph2$pgp) |
               entire(ceph1$parents == ceph2$mgp) |
               entire(ceph2$parents == ceph1$pgp) |
-              entire(ceph2$parents == ceph1$mgp)){
+              entire(ceph2$parents == ceph1$mgp)) {
       # When parents of one proband are the grandparents of the other
       relation <- "Full-Avuncular"
-    } else if(!isEmpty(intersect(ceph1$parents, c(ceph2$pgp, ceph2$mgp))) |
-              !isEmpty(intersect(ceph2$parents, c(ceph1$pgp, ceph1$mgp)))){
+    } else if (!isEmpty(intersect(ceph1$parents, c(ceph2$pgp, ceph2$mgp))) |
+              !isEmpty(intersect(ceph2$parents, c(ceph1$pgp, ceph1$mgp)))) {
       # When at least one parent of a proband is the grandparent of the other
       relation <- "Avuncular - Other"
-    } else if(kin$kinship[i] > 0){
+    } else if (kin$kinship[i] > 0) {
       relation <- "Other"
     } else{
       relation <- "No Relation"
     }
-    
+
     r <- c(r, relation)
-    
-    if(!is.null(updateProgress)){
+
+    if (!is.null(updateProgress)) {
       updateProgress()
     }
   }
   kin["relation"] <- r
   return(kin)
 }
-
-entire <- function(v){
-  # Returns true if every member of the vector is true
-  # Considers NA values the same as false
+#' Returns \code{TRUE} if every member of the vector is \code{TRUE}.
+#'
+#' Considers NA values the same as false
+#' @param v logical vector
+#' @export
+entire <- function(v) {
   v <- all(v)
-  v <- if(is.na(v)) FALSE else v
+  v <- if (is.na(v)) FALSE else v
   return(v)
 }
 
-relationClasses <- function(kin){
-  # 
+relationClasses <- function(kin) {
+  #
   # kin : data.frame {fields: id1, id2, kinship, relation}
-  # 
-  # 
-  # 
-  
+  #
+  #
+  #
+
   rel.class <- c("Self", "Parent-Offspring", "Full-Siblings", "Half-Siblings",
                  "Grandparent-Grandchild", "Full-Cousins", "Cousin - Other",
                  "Full-Avuncular", "Avuncular - Other", "Other", "No Relation")
-  
+
   kin <- kin[kin$relation != "Self", ]
   r <- as.data.frame(table(kin$relation))
   colnames(r) <- c("Relationship Class", "Frequency")
   #r$Frequency <- r$Frequency/2
-  
+
   rel.class <- rel.class[rel.class %in% r[, "Relationship Class"]]
   return(r[match(rel.class, r[, "Relationship Class"]),])
 }
@@ -248,21 +252,21 @@ relationClasses <- function(kin){
 ###############################################################################
 
 
-# filterKinDuplicates <- function(kin){
+# filterKinDuplicates <- function(kin) {
 #   # data.frame {id1, id2, kinship}
 #   pairs <- c()
 #   keep <- c()
-#   
-#   for(i in 1:nrow(kin)){
-#     pair <- paste(kin$id1[i], kin$id2[i], sep="")
-#     rev.pair <- paste(kin$id2[i], kin$id1[i], sep="")
-#     
-#     if(!(rev.pair %in% pairs)){
+#
+#   for (i in 1:nrow(kin)) {
+#     pair <- paste(kin$id1[i], kin$id2[i], sep = "")
+#     rev.pair <- paste(kin$id2[i], kin$id1[i], sep = "")
+#
+#     if (!(rev.pair %in% pairs)) {
 #       pairs <- c(pairs, pair)
 #       keep <- c(keep, i)
 #     }
 #   }
-#   
+#
 #   return(kin[keep, ])
 # }
 
