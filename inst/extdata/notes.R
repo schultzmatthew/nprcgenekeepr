2058cee782c5704a4cf4eabf3581dc21fe593939
rmsharp <rmsharp@me.com>
2017-03-14 11:39:20 -0500

    Added note.R file.

---
+++ inst/extdata/notes.R
@@ -0,0 +1,13 @@
+#' Notes file containing plans, thoughts and speculations.
+#'
+#' 1. Add a age-sex pyramide plot to learn how to use the shiny interface
+#' conventions currently in use.
+#' 2. Add the ability to detect where it is so that ONPRC or SNPRC logos and
+#' tabs are displayed appropriately.
+#' 3. Add ability to pull data directly from LabKey
+#' 4. Integrate the shiny application with LabKey.
+#' 5. Add breeding productivity measure calculations. For example, (a) number of
+#' offspring per year of exposure to male or female. (b) Number of months since
+#' last birth. (c) Number of months between births with range and standard
+#' deviation.
+#'
