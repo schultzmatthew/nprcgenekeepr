2791602f150d2585703957a8598b030a867e9726
rmsharp <rmsharp@me.com>
2017-04-03 22:53:28 -0500

    learning how to use source() in shiny

--- inst/application/ui_pyramid_plot.R
+++ inst/application/ui_pyramid_plot.R
@@ -1,5 +1,4 @@
-tabPanel("Pyramid Plot",
-
+#tabPanel("Pyramid Plot",
          div(style = "min-width:1000px",
              # Side Panel
              div(style = paste("float: left; width: 400px; height: 450px; padding: 10px;",
@@ -8,9 +7,8 @@ tabPanel("Pyramid Plot",
                                "border-radius: 25px; box-shadow: 0 0 5px 2px #888"),
                  includeHTML("../extdata/pyramid_plot.html")
              ),
-
              # Main Panel
              div(style = "margin-left:425px;padding:10px;",
                  get_pyramid_plot())
-         ))
+         )#)
 
