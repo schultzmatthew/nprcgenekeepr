d57e5604d97f569c19bf7b82cf507900ca5e33ef
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-01 16:42:04 -0500

    additional testing code.

--- vignettes/testingScript.R
+++ vignettes/testingScript.R
@@ -20,26 +20,33 @@
 # p <- trimPedigree(probands, p, removeUninformative = FALSE,
 #                   addBackSingles = FALSE)
 # test <- reportGV(p, gu.iter = 100)
-# library(RODBC)
-# library(stringi)
-# conn <- odbcConnect("frogstar-vortex-animal-msharp")
-# proband_file <- stri_c("/Users/msharp/Documents/Development/R/r_workspace/",
-#                        "library/nprcmanager/inst/extdata/",
-#                        "baboon_breeders_probands.csv")
-# # probands <- sqlQuery(conn, stri_c("select id from current_data
-# #                              where location in (102.06, 102.07, 102.08)
-# #                              and at_sfbr = 'Y'"))
-# # write.csv(probands,
-# #           file = proband_file, row.names = FALSE)
-# probands <- read.csv(proband_file, header = TRUE, sep = ",",
-#                      stringsAsFactors = FALSE, na.strings = c("", "NA"),
-#                      check.names = FALSE)
-# library(animalr)
-# library(rmsutilityr)
-# probands$id <- blank_fill_ids(probands$id)
-# ped <- get_direct_ancestors(conn, probands$id)
-# ped <- add_birth_date(conn, ped)
-# ped$birth_date <- format(ped$birth_date, format = "%Y-%m-%d")
-# ped_qc <- qc.Studbook(ped)
-# p <- trimPedigree(probands, ped_qc, removeUninformative = FALSE,
-#                   addBackSingles = FALSE)
+library(RODBC)
+library(stringi)
+conn <- odbcConnect("frogstar-vortex-animal-msharp")
+proband_file <- stri_c("/Users/msharp/Documents/Development/R/r_workspace/",
+                       "library/nprcmanager/inst/extdata/",
+                       "baboon_breeders_probands.csv")
+# probands <- sqlQuery(conn, stri_c("select id from current_data
+#                              where location in (102.06, 102.07, 102.08)
+#                              and at_sfbr = 'Y'"))
+# write.csv(probands,
+#           file = proband_file, row.names = FALSE)
+probands <- read.csv(proband_file, header = TRUE, sep = ",",
+                     stringsAsFactors = FALSE, na.strings = c("", "NA"),
+                     check.names = FALSE)
+library(animalr)
+library(rmsutilityr)
+probands <- blank_fill_ids(probands$id)
+ped <- get_direct_ancestors(conn, probands)
+ped <- add_birth_date(conn, ped)
+ped$birth_date <- format(ped$birth_date, format = "%Y-%m-%d")
+ped_qc <- qc.Studbook(ped)
+p <- trimPedigree(probands, ped_qc, removeUninformative = FALSE,
+                  addBackSingles = FALSE)
+genotype <- data.frame(id = p$id[50 + 1:20], first = 10000 + 1:20,
+                        second = 20000 + 1:20, stringsAsFactors = FALSE)
+genotype_empty <- NULL
+alleles <- geneDrop(p$id, p$sire, p$dam, p$gen, genotype, n = 1000)
+gu <- calc.gu(alleles, threshold = 1, by.id = TRUE, pop = probands)
+gu <- gu[probands,]
+
