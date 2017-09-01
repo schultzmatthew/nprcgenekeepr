ca03038aa955b0614435243655a3ecd4c43e0ea8
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-01 11:20:31 -0500

    testing

--- R/testingScript.R
+++ R/testingScript.R
@@ -1,13 +1,13 @@
 library(nprcmanager)
 # write.csv(ped, file = "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/snprc_baboon_ped.csv", row.names = FALSE)
-d <- read.csv(
-  "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/BaboonLivingForMetrics.csv",
-              header = TRUE, sep = ",", stringsAsFactors = FALSE,
-              na.strings = c("", "NA"), check.names = FALSE)
-d <- read.csv(
-  "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/Example_Pedigree.csv",
-  header = TRUE, sep = ",", stringsAsFactors = FALSE,
-  na.strings = c("", "NA"), check.names = FALSE)
+# d <- read.csv(
+#   "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/BaboonLivingForMetrics.csv",
+#               header = TRUE, sep = ",", stringsAsFactors = FALSE,
+#               na.strings = c("", "NA"), check.names = FALSE)
+# d <- read.csv(
+#   "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/Example_Pedigree.csv",
+#   header = TRUE, sep = ",", stringsAsFactors = FALSE,
+#   na.strings = c("", "NA"), check.names = FALSE)
 d <- read.csv(
   "/Users/msharp/Documents/Development/R/r_workspace/library/nprcmanager/inst/extdata/snprc_baboon_ped.csv",
   header = TRUE, sep = ",", stringsAsFactors = FALSE, na.strings = c("", "NA"), check.names = FALSE)
@@ -19,3 +19,12 @@ p["gen"] <- findGeneration(p$id, p$sire, p$dam)
 probands <- p$id[p$population]
 p <- trimPedigree(probands, p)
 test <- reportGV(p, gu.iter = 100)
+library(RODBC)
+library(stringi)
+conn <- odbcConnect("frogstar-vortex-animal-msharp")
+probands <- sqlQuery(conn, stri_c("select id from current_data
+                             where location in (102.06, 102.07, 102.08)
+                             and at_sfbr = 'Y'"))
+library(animalr)
+ped <- get_direct_ancestors(conn, probandss$id)
+ped_qc <- qc.Studbook(ped)
