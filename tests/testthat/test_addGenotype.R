8ccadf7b979c5b99d5cbfefa45c5e94213ddd1e2
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-06 13:28:58 -0500

    New tests for new funcions

---
+++ tests/testthat/test_addGenotype.R
@@ -0,0 +1,22 @@
+context("addGenotype")
+library(testthat)
+library(stringi)
+qc_ped_file <- stri_c("/Users/msharp/Documents/Development/R/r_workspace/",
+                      "library/nprcmanager/inst/extdata/",
+                      "baboon_breeders_qc_ped.csv")
+
+ped <- read.csv(qc_ped_file, header = TRUE, sep = ",",
+                     stringsAsFactors = FALSE, na.strings = c("", "NA"),
+                     check.names = FALSE)
+genotype <- data.frame(id = ped$id[50 + 1:20],
+                       first_name = stri_c("first", 1:20),
+                       second_name = stri_c("second", 1:20),
+                       stringsAsFactors = FALSE)
+
+test_that("addGenotype forms correct dataframe", {
+  newPed <- addGenotype(ped, genotype)
+  expect_equal(as.character(newPed$first[newPed$id == "1X0872"]), "10001")
+  expect_equal(as.character(newPed$second[newPed$id == "1X0872"]), "10021")
+  expect_equal(as.character(newPed$first[newPed$id == "1X0945"]), "10012")
+  expect_equal(as.character(newPed$second[newPed$id == "1X0945"]), "10032")
+})
---
+++ tests/testthat/test_checkGenotypeFile.R
@@ -0,0 +1,40 @@
+context("addGenotype")
+library(testthat)
+library(stringi)
+qc_ped_file <- stri_c("/Users/msharp/Documents/Development/R/r_workspace/",
+                      "library/nprcmanager/inst/extdata/",
+                      "baboon_breeders_qc_ped.csv")
+
+ped <- read.csv(qc_ped_file, header = TRUE, sep = ",",
+                stringsAsFactors = FALSE, na.strings = c("", "NA"),
+                check.names = FALSE)
+genotype <- data.frame(id = ped$id[50 + 1:20],
+                       first_name = stri_c("first_name", 1:20),
+                       second_name = stri_c("second_name", 1:20),
+                       stringsAsFactors = FALSE)
+
+test_that("checkGenotypeFile allows correct dataframe", {
+  expect_error(checkGenotypeFile(genotype), NA)
+})
+genotype <- data.frame(id = ped$id[50 + 1:20],
+                       first_name = stri_c("first_name", 1:20),
+                       second_name = stri_c("sec:ond_name", 1:20),
+                       stringsAsFactors = FALSE)
+test_that("checkGenotypeFile detects a bad dataframe", {
+  expect_error(checkGenotypeFile(genotype))
+})
+genotype <- data.frame(id = ped$id[50 + 1:20],
+                       first_name = stri_c("first_name", 1:20),
+                       second_name = stri_c("second_name", 1:20),
+                       stringsAsFactors = FALSE)
+genotype$first_name[genotype$id == "1X1237"] <- "almost; ok"
+test_that("checkGenotypeFile detects single error", {
+  expect_error(checkGenotypeFile(genotype))
+})
+genotype <- data.frame(id = ped$id[50 + 1:20],
+                       first_name = stri_c("first_name", 1:20),
+                       second = stri_c("second_name", 1:20),
+                       stringsAsFactors = FALSE)
+test_that("checkGenotypeFile detects illegal column name", {
+  expect_error(checkGenotypeFile(genotype))
+})
