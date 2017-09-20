9fd8533794604a26ca4757fade5fef36ac7cd29a
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-20 15:10:52 -0500

    added unit test for allTrueNoNA

---
+++ tests/testthat/test_allTrueNoNA.R
@@ -0,0 +1,10 @@
+context("allTrueNoNA")
+library(testthat)
+
+test_that("allTrueNoNA judges all vectors with one or more NA value as FALSE", {
+  expect_true(allTrueNoNA(c(TRUE, TRUE, TRUE)))
+  expect_false(allTrueNoNA(c(NA, TRUE, TRUE)))
+  expect_false(allTrueNoNA(c(NA, FALSE, TRUE)))
+  expect_false(allTrueNoNA(c(NA, FALSE, FALSE)))
+  expect_false(allTrueNoNA(c(FALSE, FALSE, FALSE)))
+})
