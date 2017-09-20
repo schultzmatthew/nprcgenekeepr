481c228c38fc4f4133cb6068ad7e89c795af4e61
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-20 12:16:49 -0500

    Update version and correction of documentation for ped1_alleles

--- R/ped1_alleles.R
+++ R/ped1_alleles.R
@@ -2,23 +2,23 @@
 #'
 #' @format A dataframe with 554 rows and 6 varialbes
 #' \describe{
-#' \item{V1} {alleles assigned to the parents of the animals identified in
+#' \item{V1}{alleles assigned to the parents of the animals identified in
 #' the \code{id} column during iteration 1 of gene dropping performed by
-#' \code{geneDrop}.
-#' \item{V2} {alleles assigned to the parents of the animals identified in
+#' \code{geneDrop}.}
+#' \item{V2}{alleles assigned to the parents of the animals identified in
 #' the \code{id} column during iteration 1 of gene dropping performed by
-#' \code{geneDrop}.
-#' \item{V3} {alleles assigned to the parents of the animals identified in
+#' \code{geneDrop}.}
+#' \item{V3}{alleles assigned to the parents of the animals identified in
 #' the \code{id} column during iteration 1 of gene dropping performed by
-#' \code{geneDrop}.
-#' \item{V4} {alleles assigned to the parents of the animals identified in
+#' \code{geneDrop}.}
+#' \item{V4}{alleles assigned to the parents of the animals identified in
 #' the \code{id} column during iteration 1 of gene dropping performed by
-#' \code{geneDrop}.
-#' \item{id} {character vector of animal IDs provided to the gene dropping
-#' function \code{geneDrop}.
-#' \item{parent} {the parent type ("sire" or "dam") of the parent who supplied
+#' \code{geneDrop}.}
+#' \item{id}{character vector of animal IDs provided to the gene dropping
+#' function \code{geneDrop}.}
+#' \item{parent}{the parent type ("sire" or "dam") of the parent who supplied
 #' the alleles as assigned during each of the 4 gene dropping iterations
-#' performed by \code{geneDrop}.
+#' performed by \code{geneDrop}.}
 #' }
 #' @source example baboon pedigree file provided by Deborah Newman,
 #' Southwest National Primate Center.
