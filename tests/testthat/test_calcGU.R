ef91cbd4ae7681afe482a0fa07182374623c7978
rmsharp <rmsharp@me.com>
2017-09-20 21:47:50 -0500

    merge edits

--- tests/testthat/test_calcGU.R
+++ tests/testthat/test_calcGU.R
@@ -2,14 +2,6 @@ context("calcGU")
 library(testthat)
 data("ped1_alleles")
 test_that("calcGU forms dataframe with correct calculations", {
-<<<<<<< HEAD
-  gu_1 <- calcGU(alleles, threshold = 1, byID = FALSE, pop = NULL)
-  gu_3 <- calcGU(alleles, threshold = 3, byID = FALSE, pop = NULL)
-  expect_equal(length(gu_1$gu[gu_1$gu == 50]), 110)
-  expect_equal(length(gu_3$gu[gu_3$gu == 50]), 43)
-  gu_1 <- calcGU(alleles, threshold = 2, byID = TRUE, pop = NULL)
-  gu_3 <- calcGU(alleles, threshold = 3, byID = FALSE, pop = alleles$id[20:60])
-=======
   gu_1 <- calcGU(ped1_alleles, threshold = 1, byID = FALSE, pop = NULL)
   gu_3 <- calcGU(ped1_alleles, threshold = 3, byID = FALSE, pop = NULL)
   expect_equal(length(gu_1$gu[gu_1$gu == 50]), 110)
@@ -17,7 +9,6 @@ test_that("calcGU forms dataframe with correct calculations", {
   gu_1 <- calcGU(ped1_alleles, threshold = 2, byID = TRUE, pop = NULL)
   gu_3 <- calcGU(ped1_alleles, threshold = 3, byID = FALSE,
                  pop = ped1_alleles$id[20:60])
->>>>>>> 128608fb032d578c2f3b5fe6b5dc4e9635a6ac43
   expect_equal(length(gu_1$gu[gu_1$gu == 50]), 53)
   expect_equal(length(gu_3$gu[gu_3$gu == 50]), 0)
 })
