115a20395129beb5beff2ba3bc5cab5907aa78d4
rmsharp <rmsharp@me.com>
2017-09-20 23:27:52 -0500

    Complete unit tests for assignAlleles()

--- tests/testthat/test_assignAlleles.R
+++ tests/testthat/test_assignAlleles.R
@@ -1,21 +1,5 @@
 context("assignAlleles")
 library(testthat)
-pedOne <- data.frame(id = c("s1", "d1", "s2", "d2", "o1", "o2", "o3", "o4"),
-                     sire = c(NA, "s0", "s4", NA, "s1", "s1", "s2", "s2"),
-                     dam = c(NA, "d0", "d4", NA, "d1", "d2", "d2", "d2"),
-                     sex = c("M", "F", "M", "F", "F", "F", "F", "M"),
-                     stringsAsFactors = FALSE)
-pedTwo <- data.frame(id = c("s1", "d1", "s2", "d2", "o1", "o2", "o3", "o4"),
-                     sire = c(NA, "s0", "s4", NA, "s1", "s1", "s2", "s2"),
-                     dam = c("d0", "d0", "d4", NA, "d1", "d2", "d2", "d2"),
-                     sex = c("M", "F", "M", "F", "F", "F", "F", "M"),
-                     stringsAsFactors = FALSE)
-
-pedThree <- data.frame(id = c("s1", "d1", "s2", "d2", "o1", "o2", "o3", "o4"),
-                       sire = c("s0", "s0", "s4", NA, "s1", "s1", "s2", "s2"),
-                       dam = c(NA, "d0", "d4", NA, "d1", "d2", "d2", "d2"),
-                       sex = c("M", "F", "M", "F", "F", "F", "F", "M"),
-                       stringsAsFactors = FALSE)
 alleles <- list(alleles = list(), counter = 1)
 test_that("assignAlleles assigns alleles correctly", {
   expect_error(assignAlleles(alleles, parentType = "sire", parent = "s1",
@@ -23,4 +7,25 @@ test_that("assignAlleles assigns alleles correctly", {
   alleles_2 <- assignAlleles(alleles, parentType = "sire", parent = NA,
                              id = "o1", n = 4)
   expect_equal(alleles_2$alleles$o1$sire, rep(1, 4))
+  alleles <- alleles_2
+  alleles_3 <- assignAlleles(alleles, parentType = "dam", parent = NA,
+                             id = "o1", n = 4)
+  expect_equal(alleles_3$alleles$o1$dam, rep(2, 4))
+  alleles <- alleles_3
+  alleles_4 <- assignAlleles(alleles, parentType = "sire", parent = NA,
+                             id = "o2", n = 4)
+  expect_equal(alleles_4$alleles$o2$sire, rep(3, 4))
+  alleles <- alleles_4
+  alleles_5 <- assignAlleles(alleles, parentType = "dam", parent = NA,
+                             id = "o2", n = 4)
+  expect_equal(alleles_5$alleles$o2$dam, rep(4, 4))
+  alleles <- alleles_5
+  alleles_6 <- assignAlleles(alleles, parentType = "dam", parent = "o1",
+                             id = "o3", n = 4)
+  expect_true(all(alleles_6$alleles$o3$dam %in% c(1, 2)))
+  alleles <- alleles_6
+  alleles_7 <- assignAlleles(alleles, parentType = "dam", parent = "o2",
+                             id = "o3", n = 4)
+  expect_true(all(alleles_6$alleles$o3$sire %in% c(3, 4)))
+
 })
