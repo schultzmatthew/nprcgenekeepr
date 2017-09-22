a88fb606a48da6978e9d03664bea8c5f40b1dc4c
rmsharp <rmsharp@me.com>
2017-09-21 22:10:43 -0500

    Added unit test for chooseDate()

---
+++ tests/testthat/test_chooseDate.R
@@ -0,0 +1,22 @@
+context("hasGenotype")
+library(testthat)
+library(lubridate)
+someDates <- mdy(paste0(sample(1:12, 10, replace = TRUE), "-",
+                    sample(1:28, 10, replace = TRUE), "-",
+                    sample(seq(0, 15, by = 3), 10, replace = TRUE) + 2000))
+test_that("chooseDate picks the correct date", {
+expect_equal(chooseDate(someDates[1], NA, earlier = TRUE),
+             someDates[1])
+  expect_equal(chooseDate(NA, someDates[2], earlier = TRUE),
+               someDates[2])
+  expect_equal(chooseDate(someDates[1], someDates[2], earlier = TRUE),
+               someDates[2])
+  expect_equal(chooseDate(someDates[1], someDates[2], earlier = FALSE),
+               someDates[1])
+  expect_equal(chooseDate(someDates[2], someDates[1], earlier = TRUE),
+               someDates[2])
+  expect_equal(chooseDate(someDates[2], someDates[1], earlier = FALSE),
+               someDates[1])
+  expect_equal(chooseDate(someDates[2], someDates[2], earlier = TRUE),
+               someDates[2])
+})
