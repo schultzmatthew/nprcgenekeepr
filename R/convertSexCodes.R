6274f2dc07735c597e8d4357dd03bf8c06e28d92
rmsharp <rmsharp@me.com>
2017-09-21 21:09:26 -0500

    Added unit test for convertSexCodes(). Converted ignore.herm to ignoreHerm

--- R/convertSexCodes.R
+++ R/convertSexCodes.R
@@ -12,10 +12,10 @@
 #' \item{U} {-- replacing "UNKNOWN" or "3"}}
 #'
 #' @param sex factor indicating sex of a set of individuals
-#' @param ignore.herm logical flag indicating if hermaphrodites should be
+#' @param ignoreHerm logical flag indicating if hermaphrodites should be
 #' treated as unknown sex ("U"), default is \code{TRUE}.
 #' @export
-convertSexCodes <- function(sex, ignore.herm = TRUE) {
+convertSexCodes <- function(sex, ignoreHerm = TRUE) {
   sex <- toupper(sex)
   sex[is.na(sex)] <- "U"
 
@@ -23,7 +23,7 @@ convertSexCodes <- function(sex, ignore.herm = TRUE) {
   sex[sex %in% c("FEMALE", "F", "2")] <- "F"
   sex[sex %in% c("UNKNOWN", "U", "3")] <- "U"
 
-  if (ignore.herm) {
+  if (ignoreHerm) {
     sex[sex %in% c("HERMAPHRODITE", "H", "4")] <- "U"
   } else {
     sex[sex %in% c("HERMAPHRODITE", "H", "4")] <- "H"
