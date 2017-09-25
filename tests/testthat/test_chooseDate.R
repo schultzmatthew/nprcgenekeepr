ea7c34e04d9cb2e8daeb6a0dc8c4af03608dbc63
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-25 10:21:50 -0500

    Corrected wrong label and added set.seed(10), which was what was originally used and some how removed.

--- tests/testthat/test_chooseDate.R
+++ tests/testthat/test_chooseDate.R
@@ -1,11 +1,12 @@
-context("hasGenotype")
+context("chooseDate")
 library(testthat)
 library(lubridate)
+set.seed(10)
 someDates <- mdy(paste0(sample(1:12, 10, replace = TRUE), "-",
                     sample(1:28, 10, replace = TRUE), "-",
                     sample(seq(0, 15, by = 3), 10, replace = TRUE) + 2000))
 test_that("chooseDate picks the correct date", {
-expect_equal(chooseDate(someDates[1], NA, earlier = TRUE),
+  expect_equal(chooseDate(someDates[1], NA, earlier = TRUE),
              someDates[1])
   expect_equal(chooseDate(NA, someDates[2], earlier = TRUE),
                someDates[2])
