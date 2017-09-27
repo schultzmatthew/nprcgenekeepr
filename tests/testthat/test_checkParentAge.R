b7af2570c8fd5304cab20c82f337cd77a0ba6ce5
rmsharp <rmsharp@me.com>
2017-09-26 22:54:17 -0500

    Increased coverage of test_checkParentAge() to get some underaged sires.

--- tests/testthat/test_checkParentAge.R
+++ tests/testthat/test_checkParentAge.R
@@ -6,10 +6,18 @@ test_that("checkParentAge identifies the over aged parents", {
   underAgeThree <- checkParentAge(baboonPed, minParentAge = 3)
   underAgeFive <- checkParentAge(baboonPed, minParentAge = 5)
   underAgeSix <- checkParentAge(baboonPed, minParentAge = 6)
+  underAgeTen <- checkParentAge(baboonPed, minParentAge = 10)
   expect_equal(nrow(underAgeTwo), 0)
   expect_equal(nrow(underAgeThree), 0)
   expect_equal(nrow(underAgeFive), 1)
   expect_equal(nrow(underAgeSix), 6)
   expect_true(all(underAgeSix$dam %in% c("15057", "16135", "1C0321", "8143",
                                          "8846", "9859")))
+  expect_true(all(underAgeTen$sire[underAgeTen$sireAge < 10 &
+                                     !is.na(underAgeTen$sireAge)] %in%
+                    c("15797", "13644", "26318", "12656", "14991", "26318",
+                      "26318", "28279", "28279", "15010", "1X1672", "1X0832",
+                      "1X3162", "1X0839", "1X1672", "1X1126", "1X1126",
+                      "1X1126", "1X1672", "1X2816", "1X3818", "1X3026",
+                      "10173", "26318", "8780", "10173", "8653")))
 })
