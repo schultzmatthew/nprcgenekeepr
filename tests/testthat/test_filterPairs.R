a8eb5769229d0e5aa9e35d2ad5c59b67e4dfe933
rmsharp <rmsharp@me.com>
2017-09-30 16:48:21 -0500

    Correct typographical error.

--- tests/testthat/test_filterPairs.R
+++ tests/testthat/test_filterPairs.R
@@ -19,5 +19,5 @@ test_that("filterPairs removes the correct pairs", {
   expect_equal(nrow(kinMM), 7)
   expect_equal(nrow(kinFF[kinFF$id1 == "B", kinFF$id2 == "E", ]), 0)
   expect_equal(nrow(kinFF[kinFF$id1 == "B", kinFF$id2 == "F", ]), 0)
-  expect_equal(nrow(kinFF[kinMM$id1 == "A", kinFF$id2 == "D", ]), 0)
+  expect_equal(nrow(kinMM[kinMM$id1 == "A", kinMM$id2 == "D", ]), 0)
 })
