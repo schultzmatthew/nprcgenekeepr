dc1f68810c93421fa98f8b0dfad59d9a4839fe92
rmsharp <rmsharp@me.com>
2017-10-01 16:51:31 -0500

    added to checkGenotypeFile() unit test

--- tests/testthat/test_checkGenotypeFile.R
+++ tests/testthat/test_checkGenotypeFile.R
@@ -17,6 +17,20 @@ genotype <- data.frame(id = ped$id[50 + 1:20],
 test_that("checkGenotypeFile allows correct dataframe", {
   expect_error(checkGenotypeFile(genotype), NA)
 })
+test_that("checkGenotypeFile disallows dataframe with < 3 columns", {
+  expect_error(checkGenotypeFile(genotype[ , c("id", "first_name")]),
+               "Genotype file must have at least three columns.")
+})
+test_that("checkGenotypeFile ensures 'id' as the first column.", {
+  names(genotype) <- c("ego", "first_name", "second_name")
+  expect_error(checkGenotypeFile(genotype),
+               "Genotype file must have 'id' as the first column.")
+})
+test_that("checkGenotypeFile ensures 'id' as the first column.", {
+  names(genotype) <- c("id", "first", "second_name")
+  expect_error(checkGenotypeFile(genotype),
+               "Genotype file cannot have a column named 'first' or 'second'.")
+})
 genotype <- data.frame(id = ped$id[50 + 1:20],
                        first_name = stri_c("first_name", 1:20),
                        second_name = stri_c("sec:ond_name", 1:20),
