d9f7550e52ce7dbd779363f9f1d723714fe8e5ce
rmsharp <rmsharp@me.com>
2017-10-02 02:00:11 -0500

    Added test to get complete coverage for unit tests of convertRelationships()

--- tests/testthat/test_convertRelationships.R
+++ tests/testthat/test_convertRelationships.R
@@ -1,21 +1,27 @@
 context("convertRelationships")
 library(testthat)
 ped <- structure(
-  list(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"),
-       sire = c(NA, NA, "A", "A", NA, "D", "D", "A", "A", NA, NA, "C"),
-       dam = c(NA, NA, "B", "B", NA, "E", "E", "B", "J", NA, NA, "K"),
-       sex = c("M", "F", "M", "M", "F", "F", "F", "M", "F", "F", "F", "F"),
-       gen = c(0, 0, 1, 1, 0, 2, 2, 1, 1, 0, 0, 1),
-       population = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
-                      TRUE, TRUE, TRUE)),
+  list(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+              "N", "O", "P", "Q"),
+       sire = c("Q", NA, "A", "A", NA, "D", "D", "A", "A", NA, NA, "C", "A",
+                NA, NA, "M", NA),
+       dam = c(NA, NA, "B", "B", NA, "E", "E", "B", "J", NA, NA, "K", "N",
+               NA, NA, "O", NA),
+       sex = c("M", "F", "M", "M", "F", "F", "F", "M", "F", "F", "F", "F", "M",
+               "F", "F", "F", "M"),
+       gen = c(1, 1, 2, 2, 1, 3, 3, 2, 2, 1, 1, 2, 1, 1, 2, 3, 0),
+       population = rep(TRUE, 17)),
   .Names = c("id", "sire", "dam", "sex", "gen", "population"),
-  row.names = c(NA, -12L),
+  row.names = c(NA, -17L),
   class = "data.frame")
 kmat <- kinship(ped$id, ped$sire, ped$dam, ped$gen, sparse = FALSE)
-ids <- c("A", "B", "D", "E", "F", "G", "I", "L")
+ids <- c("A", "B", "D", "E", "F", "G", "I", "J", "L", "M", "O", "P")
 relIds <- convertRelationships(kmat, ped, ids)
 rel <- convertRelationships(kmat, ped, NULL)
-
+data("baboonPed")
+bkmat <- kinship(baboonPed$id, baboonPed$sire, baboonPed$dam, baboonPed$gen,
+                 sparse = FALSE)
+relBIds <- convertRelationships(bkmat, baboonPed, c("1_0221", "33164"))
 test_that("convertRelationships makes correct transformations", {
   expect_equal(relIds$id1[relIds$id1 %in% rel$id1], relIds$id1)
   expect_true(all(rel$id1[rel$id1 %in% relIds$id1] %in% relIds$id1))
@@ -40,4 +46,8 @@ test_that("convertRelationships makes correct transformations", {
                "Avuncular - Other")
   expect_equal(relIds$relation[relIds$id1 == "F" & relIds$id2 == "L"],
                "Full-Cousins")
+  expect_equal(rel$relation[rel$id1 == "L" & rel$id2 == "P"],
+               "Cousin - Other")
+  expect_equal(relBIds$relation[relBIds$id1 == "1_0221" & relBIds$id2 == "33164"],
+               "Other")
 })
