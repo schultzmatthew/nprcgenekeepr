c9cfac8240c6acf324d48870fd07bb48c296bfe0
rmsharp <rmsharp@me.com>
2017-10-04 00:40:59 -0500

    Adapted unit tests to use data("smallPed")

--- tests/testthat/test_convertRelationships.R
+++ tests/testthat/test_convertRelationships.R
@@ -1,19 +1,7 @@
 context("convertRelationships")
 library(testthat)
-ped <- structure(
-  list(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-              "N", "O", "P", "Q"),
-       sire = c("Q", NA, "A", "A", NA, "D", "D", "A", "A", NA, NA, "C", "A",
-                NA, NA, "M", NA),
-       dam = c(NA, NA, "B", "B", NA, "E", "E", "B", "J", NA, NA, "K", "N",
-               NA, NA, "O", NA),
-       sex = c("M", "F", "M", "M", "F", "F", "F", "M", "F", "F", "F", "F", "M",
-               "F", "F", "F", "M"),
-       gen = c(1, 1, 2, 2, 1, 3, 3, 2, 2, 1, 1, 2, 1, 1, 2, 3, 0),
-       population = rep(TRUE, 17)),
-  .Names = c("id", "sire", "dam", "sex", "gen", "population"),
-  row.names = c(NA, -17L),
-  class = "data.frame")
+data("smallPed")
+ped <- smallPed
 kmat <- kinship(ped$id, ped$sire, ped$dam, ped$gen, sparse = FALSE)
 ids <- c("A", "B", "D", "E", "F", "G", "I", "J", "L", "M", "O", "P")
 relIds <- convertRelationships(kmat, ped, ids)
--- tests/testthat/test_createPedTree.R
+++ tests/testthat/test_createPedTree.R
@@ -1,19 +1,7 @@
 context("createPedTree")
 library(testthat)
-ped <- structure(
-  list(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-              "N", "O", "P", "Q"),
-       sire = c("Q", NA, "A", "A", NA, "D", "D", "A", "A", NA, NA, "C", "A",
-                NA, NA, "M", NA),
-       dam = c(NA, NA, "B", "B", NA, "E", "E", "B", "J", NA, NA, "K", "N",
-               NA, NA, "O", NA),
-       sex = c("M", "F", "M", "M", "F", "F", "F", "M", "F", "F", "F", "F", "M",
-               "F", "F", "F", "M"),
-       gen = c(1, 1, 2, 2, 1, 3, 3, 2, 2, 1, 1, 2, 1, 1, 2, 3, 0),
-       population = rep(TRUE, 17)),
-  .Names = c("id", "sire", "dam", "sex", "gen", "population"),
-  row.names = c(NA, -17L),
-  class = "data.frame")
+data("smallPed")
+ped <- smallPed
 pedTree <- createPedTree(ped)
 test_that("createPedTree correctly breaks up pedigrees", {
   for (id in ped$id) {
