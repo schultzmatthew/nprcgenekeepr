09bf49594b0792af9dd0f82990e3562d0f220679
R. Mark Sharp <msharp@TxBiomed.org>
2017-10-04 16:55:36 -0500

    Added tolerence to expect_equal()

--- tests/testthat/test_getPedMaxAge.R
+++ tests/testthat/test_getPedMaxAge.R
@@ -17,6 +17,6 @@ pedOne <- data.frame(ego_id = c("s1", "d1", "s2", "d2", "o1", "o2", "o3", "o4"),
 pedOne$age <- (now() - as.POSIXct(pedOne$birth)) / dyears(1)
 
 test_that("getPedMaxAge finds max age", {
-  expect_equal(getPedMaxAge(pedOne), 17.227146)
+  expect_equal(getPedMaxAge(pedOne), 17.227146, tolerance = 0.01)
   expect_equal(getPedMaxAge(pedOne[c(-1, -2), ]), 11.48742, tolerance = 0.01)
 })
