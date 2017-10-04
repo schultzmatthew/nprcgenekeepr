a88eb31118787d88de164571cbcd45ea644e053d
rmsharp <rmsharp@me.com>
2017-10-04 00:43:16 -0500

    Made a note to indicate the underlying function makeLoop() does not work.

---
+++ tests/testthat/test_findLoops.R
@@ -0,0 +1,10 @@
+context("findLoops")
+library(testthat)
+data("smallPed")
+ped <- smallPed
+data("smallPedTree")
+pedTree <- smallPedTree
+loops <- findLoops(pedTree)
+## No test performed
+## Calls makesLoop(), which does not work. It is looking for intersection
+## between dams and sires instead of repeats of dams and repeats of sires.
