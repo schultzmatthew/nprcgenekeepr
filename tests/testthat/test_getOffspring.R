d464ef63d14383f81d94ec01c0f560fcbee90fe9
R. Mark Sharp <msharp@TxBiomed.org>
2017-10-04 13:20:00 -0500

    unit test for getOffspring()

---
+++ tests/testthat/test_getOffspring.R
@@ -0,0 +1,11 @@
+context("getOffspring")
+library(testthat)
+data("smallPed")
+test_that("getOffspring returns the correct IDs", {
+  expect_equal(getOffspring(smallPed, ids = c("A", "B")),
+               c("C", "D", "H", "I", "M"))
+  expect_equal(getOffspring(smallPed, ids = c("M")),
+               c("P"))
+  expect_equal(getOffspring(smallPed, ids = c("Q")),
+               c("A"))
+})
