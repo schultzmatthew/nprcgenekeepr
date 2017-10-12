ba6e58d1d439bfba0a4356b66c6a8353ff86b38b
rmsharp <rmsharp@me.com>
2017-10-12 15:55:33 -0500

    Change linter being used to accept camelCase

--- R/fillBins.R
+++ R/fillBins.R
@@ -1,30 +1,30 @@
-#' Fill bins represented by list of two lists \code{males} and \code{females}.
+#maleBins Fill bins represented by list of two lists \code{males} and \code{females}.
 #'
 #'
-#' @param age_dist dataframe with \code{sex} and \code{age} columns
-#' @param lower_ages integer vector of lower age boundaries; must be the same
-#' length as \code{upper_ages}
-#' @param upper_ages integer vector of upper age boundaries; must be the same
-#' length as \code{lower_ages}
+#' @param ageDist dataframe with \code{sex} and \code{age} columns
+#' @param lowerAges integer vector of lower age boundaries; must be the same
+#' length as \code{upperAges}
+#' @param upperAges integer vector of upper age boundaries; must be the same
+#' length as \code{lowerAges}
 #' @export
 fillBins <- function(
-  age_dist, lower_ages,
-  upper_ages = NULL) {
-  if (is.null(upper_ages))
-    upper_ages <- c(lower_ages[-1], 100)
-  male_bins <- c()
-  female_bins <- c()
-  for (bin in seq_along(lower_ages)) {
-    male_bins <- c(male_bins,
-                   nrow(age_dist[age_dist$sex == 'M' &
-                                   age_dist$age >= lower_ages[bin] &
-                                   age_dist$age < upper_ages[bin] &
-                                   !is.na(age_dist$age), ]))
-    female_bins <- c(female_bins,
-                     nrow(age_dist[age_dist$sex == 'F' &
-                                     age_dist$age >= lower_ages[bin] &
-                                     age_dist$age < upper_ages[bin] &
-                                     !is.na(age_dist$age), ]))
+  ageDist, lowerAges,
+  upperAges = NULL) {
+  if (is.null(upperAges))
+    upperAges <- c(lowerAges[-1], 100)
+  maleBins <- c()
+  femaleBins <- c()
+  for (bin in seq_along(lowerAges)) {
+    maleBins <- c(maleBins,
+                   nrow(ageDist[ageDist$sex == "M" &
+                                   ageDist$age >= lowerAges[bin] &
+                                   ageDist$age < upperAges[bin] &
+                                   !is.na(ageDist$age), ]))
+    femaleBins <- c(femaleBins,
+                     nrow(ageDist[ageDist$sex == "F" &
+                                     ageDist$age >= lowerAges[bin] &
+                                     ageDist$age < upperAges[bin] &
+                                     !is.na(ageDist$age), ]))
   }
-  list(males = male_bins, females = female_bins)
+  list(males = maleBins, females = femaleBins)
 }
