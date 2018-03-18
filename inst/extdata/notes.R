932ba4558d9416408ce3f4e9d3aeda71c07ebfc7
rmsharp <rmsharp@me.com>
2018-03-17 22:56:08 -0500

    Added things done and something to do.

--- inst/extdata/notes.R
+++ inst/extdata/notes.R
@@ -10,3 +10,8 @@
 #' offspring per year of exposure to male or female. (b) Number of months since
 #' last birth. (c) Number of months between births with range and standard
 #' deviation.
+#' 6. Add ability to have commas or semicolons between IDs in filter for Genetic
+#' Value Analysis output table. --- Done
+#' 7. Fix layout of "Enter the number of simulations for the gene-drop analysis:"
+#' and "Enter the genome uniqueness threshold:" so that they are level with each
+#' other.
