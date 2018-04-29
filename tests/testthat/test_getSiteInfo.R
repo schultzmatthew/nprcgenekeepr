3495d0bed8e035cca553be040ff695233fff226b
rmsharp <rmsharp@me.com>
2018-04-29 13:03:23 -0500

    Added some tests to test_getSiteInfo.R

--- tests/testthat/test_getSiteInfo.R
+++ tests/testthat/test_getSiteInfo.R
@@ -1,5 +1,5 @@
 context("getSiteInfo")
-library(testthat)
+library(stringi)
 test_that("getSiteInfo at least returns the right elements", {
   expect_equal(suppressWarnings(names(getSiteInfo())),
                c("center", "baseUrl", "schemaName", "folderPath", "queryName",
@@ -7,3 +7,15 @@ test_that("getSiteInfo at least returns the right elements", {
                  "version", "nodename", "machine", "login", "user",
                  "effective_user", "homeDir", "configFile"))
 })
+test_that("getSiteInfo handled Windows and non-windows opperating systems", {
+  siteInfo <- getSiteInfo()
+  if (stri_detect_fixed(toupper(siteInfo$sysname), "WIND")) {
+    expect_equal(siteInfo$homeDir, paste0("/Users/", siteInfo$user))
+    expect_equal(siteInfo$configFile, paste0(paste0("/Users/", siteInfo$user),
+                                    "_nprcmanager_config"))
+  } else {
+    expect_equal(siteInfo$homeDir, "~/")
+    expect_equal(siteInfo$configFile, "~/.nprcmanager_config")
+  }
+})
+
