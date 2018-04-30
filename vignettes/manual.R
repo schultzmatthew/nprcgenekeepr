a37c3964306ad55da799ad90adc563ac9f237403
rmsharp <rmsharp@me.com>
2018-04-30 10:14:51 -0500

    Removed unneeded file generated during documentation creation

--- vignettes/manual.R
+++
@@ -1,8 +0,0 @@
-## ----gh-installation, eval = FALSE---------------------------------------
-#  install.packages("devtools")
-#  devtools::install_github("rmsharp/nprcmanager")
-
-## ----example, eval = FALSE-----------------------------------------------
-#  library(nprcmanager)
-#  runManager()
-
