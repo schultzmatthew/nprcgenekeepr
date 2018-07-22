b450b908390053bfaa5b2e0b38e44444151437f7
rmsharp <rmsharp@me.com>
2018-07-21 20:51:33 -0500

    Added coverage of updateProgress() calls in test_reportGV.R

--- tests/testthat/test_reportGV.R
+++ tests/testthat/test_reportGV.R
@@ -14,3 +14,20 @@ test_that("reportGV forms correct genetic value report", {
   expect_equal(gvReport$maleFounders, 20)
   expect_equal(gvReport$femaleFounders, 61)
 })
+updateProgress <- function(n = 1, detail = NULL, value = 0, reset = FALSE) {
+  "stub"
+}
+
+gvReport <- reportGV(baboonPed, gu.iter = 100, updateProgress = updateProgress)
+test_that("reportGV forms correct genetic value report with updateProgress defined", {
+  expect_equal(names(gvReport), c("report", "kinship", "gu", "fe", "fg",
+                                  "maleFounders", "femaleFounders", "total"))
+  expect_equal(names(gvReport$report),
+               c("id", "sex", "age", "birth", "exit", "population",
+                 "indivMeanKin", "zScores", "gu", "totalOffspring",
+                 "livingOffspring", "value", "rank"))
+  expect_equal(nrow(gvReport$report), nrow(baboonPed))
+  expect_equal(nrow(gvReport$gu), nrow(baboonPed))
+  expect_equal(gvReport$maleFounders, 20)
+  expect_equal(gvReport$femaleFounders, 61)
+})
