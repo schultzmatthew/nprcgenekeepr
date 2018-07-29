63147c0cc8e5137dec0c5f08b79310abe4841140
rmsharp <rmsharp@me.com>
2018-07-29 14:45:30 -0500

    testing a psuedo-mock of a function

--- tests/testthat/test_getLogo.R
+++ tests/testthat/test_getLogo.R
@@ -8,3 +8,14 @@ test_that("getLogo returns reasonalble values", {
   expect_true(logo$width > 0)
   expect_true(is.character(logo$file))
 })
+## mock getSiteInfo()
+getSiteInfo <- function() {list(center = "SNPRC")}
+logo <- suppressWarnings(getLogo())
+test_that("getLogo returns reasonalble values", {
+  expect_true(is.integer(logo$height))
+  expect_true(is.integer(logo$width))
+  expect_true(logo$height > 0)
+  expect_true(logo$width > 0)
+  expect_true(is.character(logo$file))
+})
+
