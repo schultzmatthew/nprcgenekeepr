acf4a961e6b2bccfd7201d61b1c8b4b72701ee4d
rmsharp <rmsharp@me.com>
2018-07-29 15:05:43 -0500

    testing with_mock

--- tests/testthat/test_getLogo.R
+++ tests/testthat/test_getLogo.R
@@ -9,13 +9,27 @@ test_that("getLogo returns reasonalble values", {
   expect_true(is.character(logo$file))
 })
 ## mock getSiteInfo()
-getSiteInfo <- function() {list(center = "SNPRC")}
-logo <- suppressWarnings(getLogo())
-test_that("getLogo returns reasonalble values", {
-  expect_true(is.integer(logo$height))
-  expect_true(is.integer(logo$width))
-  expect_true(logo$height > 0)
-  expect_true(logo$width > 0)
-  expect_true(is.character(logo$file))
+test_that("getLogo returns reasonalble values with SNPRC mock", {
+  with_mock(
+    getSiteInfo <- function() {list(center = "SNPRC")},
+    logo <- suppressWarnings(getLogo()),
+    expect_true(is.integer(logo$height)),
+    expect_true(is.integer(logo$width)),
+    expect_true(logo$height > 0),
+    expect_true(logo$width > 0),
+    expect_true(is.character(logo$file))
+  )
 })
+test_that("getLogo returns reasonalble values with ONPRC mock", {
+  with_mock(
+    getSiteInfo <- function() {list(center = "ONPRC")},
+    logo <- suppressWarnings(getLogo()),
+    expect_true(is.integer(logo$height)),
+    expect_true(is.integer(logo$width)),
+    expect_true(logo$height > 0),
+    expect_true(logo$width > 0),
+    expect_true(is.character(logo$file))
+  )
+})
+
 
