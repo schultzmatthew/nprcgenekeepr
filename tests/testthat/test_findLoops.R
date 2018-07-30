6a9d5972049a1b04aa944efe623112bc6cf98737
rmsharp <rmsharp@me.com>
2018-07-30 14:48:11 -0500

    Added test for findLoops

--- tests/testthat/test_findLoops.R
+++ tests/testthat/test_findLoops.R
@@ -3,8 +3,17 @@ library(testthat)
 data("smallPed")
 ped <- smallPed
 data("smallPedTree")
-pedTree <- smallPedTree
-loops <- findLoops(pedTree)
-## No test performed
-## Calls makesLoop function, which does not work. It is looking for intersection
-## between dams and sires instead of repeats of dams and repeats of sires.
+smallLoops <- findLoops(smallPedTree)
+test_that("findLoops correctly locates loops", {
+  expect_false(all(sapply(smallLoops, function(item) item)))
+  ped$sire[ped$id == "K"] <- "A"
+  pedTree <- createPedTree(ped)
+  loops <- findLoops(pedTree)
+  expect_true(loops$L)
+  expect_false(all(sapply(loops[!names(loops) == "L"], function(item) item)))
+  ped$sire[ped$id == "K"] <- "Q"
+  pedTree <- createPedTree(ped)
+  loops <- findLoops(pedTree)
+  expect_true(loops$L)
+  expect_false(all(sapply(loops[!names(loops) == "L"], function(item) item)))
+})
