6b05698437b032b19ff2d15019199cfee676688c
rmsharp <rmsharp@me.com>
2018-12-18 02:23:42 -0600

    had to remove error message check because of differences in versions.

--- tests/testthat/test_getDemographics.R
+++ tests/testthat/test_getDemographics.R
@@ -2,6 +2,5 @@ context("getDemographics")
 
 test_that("getDemographics throws an error with no LabKey session connection", {
   expect_error(expect_warning(getDemographics(),
-                              "The file should be named: ~/.nprcmanager_config."),
-               "Could not resolve host")
+                              "The file should be named: ~/.nprcmanager_config."))
 })
