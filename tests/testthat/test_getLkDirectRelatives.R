abd0f51b4043ce150d6226e3901a9855e0ede31a
R. Mark Sharp <rmsharp@me.com>
2018-12-26 21:32:12 -0600

    updated unit test with error no longer reported.

--- tests/testthat/test_getLkDirectRelatives.R
+++ tests/testthat/test_getLkDirectRelatives.R
@@ -1,7 +1,6 @@
 context("getLkDirectRelatives")
 
 test_that("getLkDirectRelatives throws an error with no LabKey session connection", {
-  expect_error(expect_warning(getLkDirectRelatives(),
-                              "The file should be named: ~/.nprcmanager_config."),
-               "attempt to set an attribute on NULL")
+  expect_warning(getLkDirectRelatives(),
+                              "The file should be named:")
 })
