5ebfa86a91abb52650b32c0e7c0cb798e6ec0362
rmsharp <rmsharp@me.com>
2019-02-09 12:31:53 -0600

    changed logo file name to remove spaces

--- R/getLogo.R
+++ R/getLogo.R
@@ -4,11 +4,11 @@
 getLogo <- function() {
   logo <- list()
   if (getSiteInfo()$center == "SNPRC") {
-    logo$file <- "../NPRCmanager 2c logo stamp w-tagline_300dpi.jpg"
+    logo$file <- "../nprcmanager_2_color_logo.jpg"
     logo$height <- 200L
     logo$width <- 350L
   } else {
-    logo$file <- "../NPRCmanager 2c logo stamp w-tagline_300dpi.jpg"
+    logo$file <- "../nprcmanager_2_color_logo.jpg"
     logo$height <- 200L
     logo$width <- 350L
   }
