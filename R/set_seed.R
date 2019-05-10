6ff11c364e729aa5f6587007d60b33af830ac066
rmsharp <rmsharp@me.com>
2019-05-09 20:10:14 -0500

    tryCatch() implementation of set_seed()

--- R/set_seed.R
+++ R/set_seed.R
@@ -2,10 +2,8 @@
 #'
 #' @param seed argument to \code{set.seed}
 set_seed <- function(seed = 1) {
-  version <- as.integer(R.Version()$major) + (as.numeric(R.Version()$minor) / 10.0)
-  if (version >= 3.6) {
-    suppressWarnings(set.seed(seed, sample.kind = "Rounding"))
-  } else {
-    set.seed(seed)
-  }
+  #version <- as.integer(R.Version()$major) + (as.numeric(R.Version()$minor) / 10.0)
+  tryCatch(suppressWarnings(set.seed(seed, sample.kind = "Rounding")),
+           warning = function() set.seed(seed),
+           error = function() set.seed(seed))
 }
