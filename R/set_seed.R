10d50d389cd9fc6d0e2025bc65a54e6c7f05f003
rmsharp <rmsharp@me.com>
2019-05-09 21:49:30 -0500

    back to do.call() version as it worked on all versions of R

--- R/set_seed.R
+++ R/set_seed.R
@@ -3,14 +3,10 @@
 #' @param seed argument to \code{set.seed}
 set_seed <- function(seed = 1) {
   version <- as.integer(R.Version()$major) + (as.numeric(R.Version()$minor) / 10.0)
-  # if (version >= 3.6) {
-  #   args <- list(seed, sample.kind = "Rounding")
-  # } else {
-  #   args <- list(seed)
-  # }
-  # suppressWarnings(do.call(set.seed, args))
   if (version >= 3.6) {
-    RNGkind(sample.kind="Rounding")
+    args <- list(seed, sample.kind = "Rounding")
+  } else {
+    args <- list(seed)
   }
-  set.seed(seed)
+  suppressWarnings(do.call(set.seed, args))
 }
