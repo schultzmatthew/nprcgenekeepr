a12015a9ac98b77300904efe21dd4fd7f1e8e164
rmsharp <rmsharp@me.com>
2019-05-09 20:56:06 -0500

    RNGkind() version of set_seed()

--- R/set_seed.R
+++ R/set_seed.R
@@ -3,10 +3,14 @@
 #' @param seed argument to \code{set.seed}
 set_seed <- function(seed = 1) {
   version <- as.integer(R.Version()$major) + (as.numeric(R.Version()$minor) / 10.0)
+  # if (version >= 3.6) {
+  #   args <- list(seed, sample.kind = "Rounding")
+  # } else {
+  #   args <- list(seed)
+  # }
+  # suppressWarnings(do.call(set.seed, args))
   if (version >= 3.6) {
-    args <- list(seed, sample.kind = "Rounding")
-  } else {
-    args <- list(seed)
+    RNGkind(sample.kind="Rounding")
   }
-  suppressWarnings(do.call(set.seed, args))
+  set.seed(seed)
 }
