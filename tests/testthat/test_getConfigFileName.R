291028657c57ea5a2af9b8225733b97836ed5855
rmsharp <rmsharp@me.com>
2019-11-20 11:01:41 -0600

    removed image directory in path

--- tests/testthat/test_getConfigFileName.R
+++ tests/testthat/test_getConfigFileName.R
@@ -5,13 +5,22 @@ library(testthat)
 
 ## These are just stub values
 sysInfoUnix <-
-  c(sysname = "Darwin", release = "17.7.0", version = "Darwin Kernel Version 17.7.0: Thu Jun 21 22:53:14 PDT 2018; root:xnu-4570.71.2~1/RELEASE_X86_64",
-    nodename = "prefect.local", machine = "x86_64", login = "msharp",
+  c(sysname = "Darwin",
+    release = "17.7.0",
+    version = "Darwin Kernel Version 17.7.0: Thu Jun 21 22:53:14 PDT 2018; root:xnu-4570.71.2~1/RELEASE_X86_64",
+    nodename = "prefect.local",
+    machine = "x86_64",
+    login = "msharp",
     user = "msharp", effective_user = "msharp")
 sysInfoWindows <-
-  c(sysname = "Windows", release = "17.7.0", version = "Darwin Kernel Version 17.7.0: Thu Jun 21 22:53:14 PDT 2018; root:xnu-4570.71.2~1/RELEASE_X86_64",
-    nodename = "prefect.local", machine = "x86_64", login = "msharp",
-    user = "msharp", effective_user = "msharp")
+  c(sysname = "Windows",
+    release = "17.7.0",
+    version = "Darwin Kernel Version 17.7.0: Thu Jun 21 22:53:14 PDT 2018; root:xnu-4570.71.2~1/RELEASE_X86_64",
+    nodename = "prefect.local",
+    machine = "x86_64",
+    login = "msharp",
+    user = "msharp",
+    effective_user = "msharp")
 test_that("getConfigFile got correct file name", {
   skip_if_not(Sys.info()[names(Sys.info()) == "user"] == "msharp")
   expect_equal(getConfigFileName(sysInfoWindows)[["configFile"]],
