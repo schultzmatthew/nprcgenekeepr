7e37c6b06fa75d653bb76d094920d41a8222c8ed
rmsharp <rmsharp@me.com>
2020-04-05 00:44:57 -0500

    Added example to checkChangedColsLst() and exported it.

--- R/checkChangedColsLst.R
+++ R/checkChangedColsLst.R
@@ -4,8 +4,28 @@
 ## This file is part of nprcgenekeepr
 #' @return Returns \code{NULL} if all fields are empty
 #' else the entire list is returned.
+#' @examples
+#' \donttest{
+#' library(nprcgenekeepr)
+#' library(lubridate)
+#' pedOne <- data.frame(ego_id = c("s1", "d1", "s2", "d2", "o1", "o2", "o3",
+#'                                 "o4"),
+#'                     `si re` = c(NA, NA, NA, NA, "s1", "s1", "s2", "s2"),
+#'                     dam_id = c(NA, NA, NA, NA, "d1", "d2", "d2", "d2"),
+#'                      sex = c("F", "M", "M", "F", "F", "F", "F", "M"),
+#'                      birth_date = mdy(
+#'                        paste0(sample(1:12, 8, replace = TRUE), "-",
+#'                               sample(1:28, 8, replace = TRUE), "-",
+#'                               sample(seq(0, 15, by = 3), 8, replace = TRUE) +
+#'                                 2000)),
+#'                      stringsAsFactors = FALSE, check.names = FALSE)
+#'
+#' errorLst <- qcStudbook(pedOne, reportErrors = TRUE, reportChanges = TRUE)
+#' checkChangedColsLst(errorLst$changedCols)
+#' }
 #' @param changedCols list with fields for each type of column change
 #' \code{qcStudbook}.
+#' @export
 checkChangedColsLst <- function(changedCols) {
   if (length(changedCols$caseChange) > 0 |
       length(changedCols$spaceRemoved) > 0 |
@@ -22,3 +42,4 @@ checkChangedColsLst <- function(changedCols) {
     return(FALSE)
   }
 }
+
