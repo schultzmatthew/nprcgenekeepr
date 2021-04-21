e502ce76c7baa1dcadf1f9182475045536006c84
rmsharp <rmsharp@me.com>
2021-04-21 10:42:59 -0500

    started new version

--- R/getVersion.R
+++ R/getVersion.R
@@ -12,8 +12,8 @@
 #' within parentheses is appended.
 #' @export
 getVersion <- function(date = TRUE) {
-  version <- "1.0.5"
-  version_date <- "20210322"
+  version <- "1.0.5.9000"
+  version_date <- "20210421"
   if (date) {
     paste0(version, " (", version_date, ")")
   } else {
