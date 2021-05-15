9ed2959e1aff3dddaacc274bba3977c51c32351f
rmsharp <rmsharp@me.com>
2021-05-15 08:41:39 -0500

    stub function for getting potential or representative parents for kinship calculations with simulated pedigrees.

---
+++ tests/testthat/test_getRepresentativeParents.R
@@ -0,0 +1,14 @@
+#' Copyright(c) 2017-2020 R. Mark Sharp
+# This file is part of nprcgenekeepr
+# context("getRepresentativeParents")
+# library(testthat)
+# library(stringi)
+# ped <- nprcgenekeepr::pedSix
+# test_that(
+#   stri_c("getPotentialParents returns the correct subsets of male ",
+#          "and females"), {
+#   parents <- getRepresentativeParents(id = "d2", ped)
+#   expect_true(all(is.element(parents$sire, c("s1", "s2"))))
+#   expect_true(all(is.element(parents$dam, c("d1"))))
+# })
+#
